273d3930e1191792c321412c6d57513af97a08ff
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-06-27 14:33:27 -0700

    Started auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,20 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login!(@user)
+      render @user
+    else
+      render json: ['Invalid username or password'], status: 401
+    end
+  end
+
+  def destroy
+    logout!
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,60 @@
+class User < ApplicationRecord
+
+ validates :username, presence: true, uniqueness: true
+ validates :session_token, :password_digest, presence: true
+ validates :password, length: { minimum: 6, allow_nil: true }
+
+ attr_reader :password
+
+ after_initialize :ensure_session_token
+
+  has_many :shops,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Shop
+
+  has_many :listings,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Listing
+
+  has_many :gateways,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Gateway
+
+  has_many :notifications,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Notification
+
+  has_many :profiles,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Profile
+
+ def self.find_by_credentials(username, password)
+   user = User.find_by(username: username)
+   user && user.is_password?(password) ? user : nil
+ end
+
+ def password=(password)
+   @password = password
+   self.password_digest = BCrypt::Password.create(password)
+ end
+
+ def is_password?(password)
+   BCrypt::Password.new(self.password_digest).is_password?(password)
+ end
+
+ def ensure_session_token
+   self.session_token ||= SecureRandom.urlsafe_base64
+ end
+
+ def reset_session_token!
+   self.session_token = SecureRandom.urlsafe_base64
+   self.save!
+   self.session_token
+ end
+
+end
